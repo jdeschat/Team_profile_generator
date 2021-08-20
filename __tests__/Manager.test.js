62018ae693931a60aecae7745fab2dd43227aa12
Joelle DesChatelets <jdeschat@gmail.com>
2021-08-20 17:12:16 -0400

    added manager class and tested its methods

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,25 @@
+const Manager = require('../lib/Manager');
+const manager = new Manager('joelle', '5768098', 'jdeschat@gmail.com', '821');
+
+test('test if we can get the constructor values for the employee object', () => {
+    expect(manager.name).toBe('joelle');
+    expect(manager.id).toBe('5768098');
+    expect(manager.email).toBe('jdeschat@gmail.com');
+    expect(manager.officeNumber).toBe('821');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(manager.getName()).toBe('joelle');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(manager.getId()).toBe('5768098');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(manager.getEmail()).toBe('jdeschat@gmail.com');
+});
+
+test('test if we can get the office number from the getOfficeNumber() method', () => {
+    expect(manager.getOfficeNumber()).toBe('821');
+});
