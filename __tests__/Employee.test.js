d6d6195cf9e79e4ebda6cf88587b3eda87e0ba66
Joelle DesChatelets <jdeschat@gmail.com>
2021-08-20 17:01:34 -0400

    added employee class and tested its methods

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee('joelle', '5768098', 'jdeschat@gmail.com');
+
+test('test if we can get the constructor values for the employee object', () => {
+    expect(employee.name).toBe('joelle');
+    expect(employee.id).toBe('5768098');
+    expect(employee.email).toBe('jdeschat@gmail.com');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(employee.getName()).toBe('joelle');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(employee.getId()).toBe('5768098');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('jdeschat@gmail.com');
+});
