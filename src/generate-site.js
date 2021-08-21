1a82cf82f4ff3863a95fffa94a7ad05dbc3d8c6f
Joelle DesChatelets <jdeschat@gmail.com>
2021-08-21 12:09:22 -0400

    passed through manager on the generated site

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,7 +1,29 @@
+const Manager = require("../lib/Manager");
+
 const generateTeam = (team) => {
-    // console.log(team);
-    // return "<p>hello</p>";
-    // return ${promptManager(team)}
+    console.log(team);
+    // Create an empty array to push html elements on to and loop through the team data
+    const html = [];
+    // Create functions for each type of employee that returns HTML data
+    const generateManager = manager => {
+        console.log(manager);
+        let managerHtml = ` <div class="card employee-card">
+           <p>ManagerInfo</p>
+
+        </div>
+        `;
+        html.push(managerHtml);
+    }
+
+    // create a loop for all of the employees
+    for (let i = 0; i < team.length; i++) {
+        if (team[i].getRole() === "Manager") {
+            generateManager(team[i]);
+        }
+    }
+
+    // join the HTML blocks
+    return html.join('');
 }
 // export function to generate entire page
 module.exports = team => {
@@ -9,7 +31,6 @@ module.exports = team => {
     return `
     <!DOCTYPE html>
 <html lang="en">
-
 
 <body>
      ${generateTeam(team)}
